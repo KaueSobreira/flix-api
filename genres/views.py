683001616326e9c129b850e6d34298ed0e9910e5
Kaue Sobreira <sobreirak29@gmail.com>
2025-12-09 20:17:55 -0300

    feat: implementado primeiro rota utilizando apenas Django Puro para fazer API

--- genres/views.py
+++ genres/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from django.http import JsonResponse
+from .models import Genre
 
 # Create your views here.
+def genre_view(request):
+    genres = Genre.objects.all()
+    genre_list = [
+        {
+            'id': genre.id,
+            'name': genre.name
+        }
+        for genre in genres
+    ]
+    return JsonResponse(genre_list, safe=False)
