d46dd85e8b13cdd68a7ddf203c5f22a67aa285b5
Kaue Sobreira <sobreirak29@gmail.com>
2025-12-15 20:44:01 -0300

    feat: add URL routing for actors, genres, movies, and reviews in separate modules

--- app/urls.py
+++ app/urls.py
@@ -1,22 +1,14 @@
 from django.contrib import admin
-from django.urls import path
-from genres.views import GenreCreateListView, GenreRetrieveUpdateDestroyView
-from actors.views import ActorCreateListView, ActorRetrieveUpdateDestroyView
-from movies.views import MovieCreateListView, MovieRetrieveUpdateDestroyView
-from reviews.views import ReviewCreateListView, ReviewRetrieveUpdateDestroy
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
 
-    path('genres/', GenreCreateListView.as_view(), name='genre-create-list'),
-    path('genres/<int:pk>', GenreRetrieveUpdateDestroyView.as_view(), name='genre-detail-view'),
+    path('api/v1/', include('genres.urls')),
 
-    path('actors/', ActorCreateListView.as_view(), name='actor-create-list'),
-    path('actors/<int:pk>', ActorRetrieveUpdateDestroyView.as_view(), name='actor-detail-view'),
+    path('api/v1/', include('actors.urls')),
 
-    path('movies/', MovieCreateListView.as_view(), name='movie-create-list'),
-    path('movies/<int:pk>', MovieRetrieveUpdateDestroyView.as_view(), name='movie-detail-view'),
+    path('api/v1/', include('movies.urls')),
 
-    path('reviews/', ReviewCreateListView.as_view(), name='review-create-list'),
-    path('reviews/<int:pk>', ReviewRetrieveUpdateDestroy.as_view(), name='review-detail-view')
+    path('api/v1/', include('reviews.urls'))
 ]
