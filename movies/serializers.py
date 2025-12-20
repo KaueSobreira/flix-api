6927fdc175bac147cc75ae7a7c0bcfb5add46603
Kaue Sobreira <sobreirak29@gmail.com>
2025-12-20 20:39:29 -0300

    feat: add validation for resume length in MovieSerializer to enforce character limit

--- movies/serializers.py
+++ movies/serializers.py
@@ -6,3 +6,13 @@ class MovieSerializer(serializers.ModelSerializer):
     class Meta:
         model = Movie
         fields = '__all__'
+
+    # def validate_release_date(self, value):
+    #     if value.year < 1990:
+    #         raise serializers.ValidationError('A data de lançamento é inferior a 1990!!')
+    #     return value
+
+    def validate_resume(self, value):
+        if len(value) > 200:
+            raise serializers.ValidationError('O Resumo não pode ser maior que 200 Caracteres.')
+        return value
