5bbf4d813a33e19e3ca6cb35c797c3eb28c46ce9
Kaue Sobreira <sobreirak29@gmail.com>
2025-12-20 21:18:28 -0300

    feat: add rate field to MovieSerializer and update resume length validation to 500 characters

--- movies/serializers.py
+++ movies/serializers.py
@@ -1,18 +1,23 @@
 from rest_framework import serializers
 from movies.models import Movie
+from reviews.models import Review
 
 class MovieSerializer(serializers.ModelSerializer):
+    rate = serializers.SerializerMethodField(read_only=True)
     
     class Meta:
         model = Movie
         fields = '__all__'
 
+    def get_rate(self, obj):
+        return 2
+
     # def validate_release_date(self, value):
-    #     if value.year < 1990:
+    #     if value.year < 1900:
     #         raise serializers.ValidationError('A data de lançamento é inferior a 1990!!')
     #     return value
 
     def validate_resume(self, value):
-        if len(value) > 200:
+        if len(value) > 500:
             raise serializers.ValidationError('O Resumo não pode ser maior que 200 Caracteres.')
         return value
