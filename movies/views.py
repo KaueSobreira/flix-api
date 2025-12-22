a725af8bbef1ff0c192a738f038d1b7c3c3c6700
Kaue Sobreira <sobreirak29@gmail.com>
2025-12-22 20:26:02 -0300

    fix: add trailing commas to permission_classes in actor, genre, and movie views for consistency and improved readability

--- movies/views.py
+++ movies/views.py
@@ -5,12 +5,12 @@ from movies.serializers import MovieSerializer
 from app.permissions import GlobalDefaultPermission
 
 class MovieCreateListView(generics.ListCreateAPIView):
-    permission_classes = (IsAuthenticated, GlobalDefaultPermission)
+    permission_classes = (IsAuthenticated, GlobalDefaultPermission,)
     queryset = Movie.objects.all()
     serializer_class = MovieSerializer
 
 class MovieRetrieveUpdateDestroyView(generics.RetrieveUpdateDestroyAPIView):
-    permission_classes = (IsAuthenticated, GlobalDefaultPermission)
+    permission_classes = (IsAuthenticated, GlobalDefaultPermission,)
     queryset = Movie.objects.all()
     serializer_class = MovieSerializer
 
